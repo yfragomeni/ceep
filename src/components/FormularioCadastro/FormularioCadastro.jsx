b4d7cb0a31525d377641547f8370fc7e205a2ee8
yfragomeni <yfragomeni@gmail.com>
2021-03-24 19:28:01 -0300

    Aula 04

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -2,13 +2,24 @@ import React from 'react';
 import './estilo.css'
 
 class FormularioCadastro extends React.Component {
+
+    constructor() {
+        super();
+        this.titulo = "";
+    }
+
+    handleMudancaTitulo(evento) {
+        this.titulo = evento.target.value;
+    } 
+
     render() {
         return (
         <form className="form-cadastro">
             <input 
             type= "text" 
             placeholder="Título"
-            className="form-cadastro_input"/>
+            className="form-cadastro_input"
+            onChange={this.handleMudancaTitulo.bind(this)}/>
             <textarea 
             rows={15}
             placeholder="Escreva sua nota..."
